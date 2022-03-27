914f99b62fd37fda392e247095b3697d5fe44a8e
Luke Guan <lukeguan@outlook.com>
2022-03-26 21:12:41 -0700

    row mapper

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,7 @@
 package com.in28minutes.database.databasedemo;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.database.databasedemo.entity.Person;
 import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
 
 @SpringBootApplication
@@ -26,7 +29,11 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 		logger.info("All users -> {}", dao.findAll());
 		logger.info("User id 10001 -> {}", dao.findById(10001));
 		logger.info("Deleting id 10002 -> {}", dao.deleteById(10002));
-
+		logger.info("Inserting id 10004 -> {}", 
+				dao.insert(new Person(10004, "Rata", "Berlin", new Date())));
+		logger.info("Updating id 10002 -> {}", 
+				dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+		
 	}
 
 }
